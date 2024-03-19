f85311e798b51d2e8dbad525fcde58a00afb08c5
JoLiKs <vonkad002@gmail.com>
2024-03-19 22:58:44 +0300

    Bug fix

--- src/main/java/com/example/spring_booking_bot/commands/ChooseTime.java
+++ src/main/java/com/example/spring_booking_bot/commands/ChooseTime.java
@@ -8,6 +8,8 @@ import com.example.spring_booking_bot.models.UserModel;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 
 import java.util.List;
 
@@ -33,10 +35,16 @@ public class ChooseTime implements WorkerCommand {
         bookModel.setTgId(update.getMessage().getFrom().getId().toString());
         bookModel.setDoctorEnum(userModel.getDoctorEnum());
         DoctorHelper.save(bookModel);
+        System.out.println("Запись к врачу");
         SendMessage sendMessage = new SendMessage();
+        ReplyKeyboardRemove rmkb = new ReplyKeyboardRemove(true);
+        rmkb.setSelective(true);
+        sendMessage.setReplyMarkup(rmkb);
         sendMessage.setChatId(update.getMessage().getChatId().toString());
         sendMessage.setText("Вы успешно записались к врачу");
         return sendMessage;
+
+
     }
 
     @Override
